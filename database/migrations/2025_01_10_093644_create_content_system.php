a19e76ccae9c21125b8709b2c1fc69211693addd
feltwine <patik.vaclav@proton.me>
2025-01-10 10:41:24 +0100

    Change migrations of posts, comments and votes into one

--- database/migrations/2025_01_10_093644_create_content_system.php
+++ database/migrations/2025_01_10_093644_create_content_system.php
@@ -28,6 +28,25 @@ return new class extends Migration
             $table->integer('order')->default(0);
             $table->timestamps();
         });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('comments')->onDelete('cascade');
+            $table->text('content');
+            $table->integer('vote_count')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained('users');
+            $table->foreignId('post_id')->unique()->constrained('posts');
+            $table->smallInteger('vote');  // Options +1, 0, -1
+            $table->unique(['user_id', 'voteable_id', 'voteable_type']);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -35,7 +54,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
-        Schema::dropIfExists('post_images');
+        Schema::dropIfExists('content_system');
     }
 };
